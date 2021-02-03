f7debda0f37c894e38b1da47eec9a20c65c0327e
svs <svs@zeppelin>
2021-02-03 03:51:08 +0200

    splashscreen example

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -3,6 +3,10 @@ module hellofx {
     requires javafx.fxml;
     requires transitive javafx.graphics;
 
-    opens org.openjfx to javafx.fxml;
+    //opens org.openjfx to javafx.fxml;
+    opens org.openjfx to javafx.fxml, javafx.graphics;
+
+    opens org.svs.dupfinder to javafx.fxml, javafx.graphics;
+
     exports org.openjfx;
 }
